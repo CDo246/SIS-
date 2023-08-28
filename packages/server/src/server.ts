c43a57b03d6c9c631ffbd5c8c5de0eee8cc2ee8f
CDo246 <celinedo9999@gmail.com>
2023-08-28 23:46:43 +1000

    Set up trpc to connect server and client

--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -1,1 +1,31 @@
-console.log("soup");
+import { inferAsyncReturnType, initTRPC } from "@trpc/server";
+import * as trpcExpress from "@trpc/server/adapters/express";
+import express from "express";
+import { appRouter } from "./api/router";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config({ path: ".env" });
+dotenv.config({ path: "../../.env" });
+
+// created for each request
+const createContext = ({
+  req,
+  res,
+}: trpcExpress.CreateExpressContextOptions) => ({}); // no context
+
+type Context = inferAsyncReturnType<typeof createContext>;
+const t = initTRPC.context<Context>().create();
+
+const app = express();
+
+app.use(cors());
+
+app.use(
+  "/trpc",
+  trpcExpress.createExpressMiddleware({
+    router: appRouter,
+    createContext,
+  })
+);
+app.listen(4000);
