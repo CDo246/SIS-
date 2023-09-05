e679538367c433561f55d18bdfd5a160f97aab85
RichieNguyen <richiepnguyen@gmail.com>
2023-09-06 05:21:51 +1000

    more additions to chat display
    - added chat bubbles
    - alternated text side every message
    - show text output as if it were generated in real time

---
+++ packages/client/src/assets/TypingText.tsx
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from 'react';
+// Inputs
+// text: Desired text to display
+// speed: Speed of text being displayed (in miliseconds per character)
+function TypingText({ text, speed }) { 
+    
+    const [displayedText, setDisplayedText] = useState('');
+    const [currentIndex, setCurrentIndex] = useState(0);
+
+    useEffect(() => {
+
+        const typingInterval = setInterval(() => {
+            if (currentIndex < text.length) {
+
+                setDisplayedText(text.slice(0, currentIndex + 1));
+                setCurrentIndex(currentIndex + 1);
+            } else {
+                clearInterval(typingInterval);
+        }
+    }, speed);
+
+    return () => {
+        clearInterval(typingInterval);
+    };
+    }, [currentIndex, text, speed]);
+
+    return <span>{displayedText}</span>;
+}
+
+export default TypingText;
--- packages/client/src/pages/MainPage.tsx
+++ packages/client/src/pages/MainPage.tsx
@@ -1,64 +1,49 @@
+import { useState } from "react";
 import { trpc } from "../utils/trpc";
 import '../index.css';
+import TypingText from '../assets/TypingText';
 
 export function MainPage() {
-<<<<<<< HEAD
-  const hello = trpc.greeting.useQuery();
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank"></a>
-        <a href="https://react.dev" target="_blank"></a>
-      </div>
-      <body>
-        <div id="footer">
-          <button>Publish</button>
-          <textarea
-            name="Enter Topic"
-            placeholder="Enter Topic Here...."
-          ></textarea>
-        </div>
-      </body>
-
-      <p>{hello.data}</p>
-    </>
-=======
-  const [messages, setMessages] = useState<string[]>([]);
+  const [messages, setMessages] = useState<{ text: string; isRight: boolean }[]>([]);
   const [message, setMessage] = useState<string>('');
 
   const handleSend = () => {
     if (message.trim() !== '') {
-        setMessages([...messages, message]);
-        setMessage('');
+      //console.log('message:', message); // debug message text
+      const newMessage = { text: message, isRight: messages.length % 2 === 0 }; // Checking whether message is sent by right or left bot
+      //console.log('newmessage:', newMessage); // debug new message text
+      setMessages([...messages, newMessage]);
+      setMessage('');
     }
 };
-  const [count, setCount] = useState(0);
-
-  const hello = trpc.greeting.useQuery();
-
   return (
     <div className="container mx-auto p-4">
-            <div className="border rounded-lg p-4 shadow-md chat-container">
-                {messages.map((msg, index) => (
-                    <div key={index} className="mb-2">{msg}</div>
-                ))}
-            </div>
-            <div className="mt-4">
-                <input
-                    type="text"
-                    className="w-full p-2 rounded-lg border"
-                    value={message}
-                    onChange={(e) => setMessage(e.target.value)}
-                />
-                <button
-                    className="bg-blue-500 text-white px-4 py-2 rounded-lg ml-2"
-                    onClick={handleSend}
-                >
-                    Send
-                </button>
-            </div>
-        </div>
->>>>>>> a526d85 (added initial chat display)
+      <div className="border rounded-lg p-4 shadow-md chat-container">
+        {messages.map((msg, index) => (
+          <div
+            key={index}
+            className={`${
+              msg.isRight ? 'right-message' : 'left-message'
+            }`}
+            >
+              <TypingText text={msg.text} speed={50} /> {/* Speed can be adjusted to be faster/slower if needed */}
+              </div>
+        ))}
+      </div>
+      <div className="mt-4">
+        <input
+          type="text"
+          className="w-full p-2 rounded-lg border"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+        />
+        <button
+          className="bg-blue-500 text-white px-4 py-2 rounded-lg ml-2"
+          onClick={handleSend}
+        >
+          Send
+        </button>
+      </div>
+    </div>
   );
 }
