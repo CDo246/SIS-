cd729642f0ccfb98aba20ab7118f35d27556d8ef
RichieNguyen <richiepnguyen@gmail.com>
2023-09-06 04:07:56 +1000

    added initial chat display

--- packages/client/src/pages/MainPage.tsx
+++ packages/client/src/pages/MainPage.tsx
@@ -1,6 +1,8 @@
 import { trpc } from "../utils/trpc";
+import '../index.css';
 
 export function MainPage() {
+<<<<<<< HEAD
   const hello = trpc.greeting.useQuery();
 
   return (
@@ -21,5 +23,42 @@ export function MainPage() {
 
       <p>{hello.data}</p>
     </>
+=======
+  const [messages, setMessages] = useState<string[]>([]);
+  const [message, setMessage] = useState<string>('');
+
+  const handleSend = () => {
+    if (message.trim() !== '') {
+        setMessages([...messages, message]);
+        setMessage('');
+    }
+};
+  const [count, setCount] = useState(0);
+
+  const hello = trpc.greeting.useQuery();
+
+  return (
+    <div className="container mx-auto p-4">
+            <div className="border rounded-lg p-4 shadow-md chat-container">
+                {messages.map((msg, index) => (
+                    <div key={index} className="mb-2">{msg}</div>
+                ))}
+            </div>
+            <div className="mt-4">
+                <input
+                    type="text"
+                    className="w-full p-2 rounded-lg border"
+                    value={message}
+                    onChange={(e) => setMessage(e.target.value)}
+                />
+                <button
+                    className="bg-blue-500 text-white px-4 py-2 rounded-lg ml-2"
+                    onClick={handleSend}
+                >
+                    Send
+                </button>
+            </div>
+        </div>
+>>>>>>> a526d85 (added initial chat display)
   );
 }
