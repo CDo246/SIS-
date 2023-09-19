0d7f7643bd2bb9469ee27724a8ee4611cab81655
RichieNguyen <richiepnguyen@gmail.com>
2023-09-20 04:52:54 +1000

    Revert "Merge branch 'richie-chatdisplay' of https://github.com/CDo246/SIS-Debate into richie-chatdisplay"
    This reverts commit 211a8ce5e11e0b953973e6bff2392dea658d6645, reversing
    changes made to ef9e26b8605a4050022b5a563603c68ddd099164.

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import { MainPage } from "./pages/MainPage";
-import { NavBar } from "./nav/NavBar.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { trpc } from "./utils/trpc";
 import { httpBatchLink } from "@trpc/client";
@@ -18,13 +17,12 @@ function App() {
           },
         }),
       ],
-    }),
+    })
   );
 
   return (
     <trpc.Provider client={trpcClient} queryClient={queryClient}>
       <QueryClientProvider client={queryClient}>
-        <NavBar />
         <MainPage />
       </QueryClientProvider>
     </trpc.Provider>
--- packages/server/src/api/router.ts
+++ packages/server/src/api/router.ts
@@ -1,5 +1,3 @@
-import { ChatCompletionMessageParam } from "openai/resources/chat";
-import { openaiKey } from "../env";
 import { publicProcedure, router } from "./trpc";
 import OpenAI from "openai";
 
@@ -11,130 +9,19 @@ export const appRouter = router({
 // This prevents us from importing server code on the client.
 export type AppRouter = typeof appRouter;
 
+
 const openai = new OpenAI({
-  apiKey: openaiKey,
+  apiKey: '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', // AHMAD'S API KEY, PLEASE DO NOT OVERUSE!
 });
 
-type Side = "for" | "against";
-
-type DebateMessage = {
-  side: Side;
-  thinking: string;
-  message: string;
-};
-
-type SysMessageParams = {
-  currentRole: string;
-  otherRole: string;
-  messageCount: number;
-  topic: string;
-  side: Side;
-};
-
-type ArgumentParams = {
-  startingSide: Side;
-  topic: string;
-  messageCount: number;
-  role1: string;
-  role2: string;
-};
-
-function flipSide(side: Side): Side {
-  return side == "for" ? "against" : "for";
-}
-
-function buildSysMessage(params: SysMessageParams) {
-  return `
-You are an experienced debater debating a topic.
-The debate consists of ${params.messageCount} messages per debater.
-The debate should wrap up before the message count, but the final verdict will be decided by a judge.
-The topic is: ${params.topic}.
-
-Your role in the debate is ${params.currentRole}, and you are arguing ${
-    params.side
-  } the topic.
-The opposing debater's role is ${
-    params.otherRole
-  }, and they are arguing ${flipSide(params.side)} the topic.
-The debate should be entertaining and understandable to read.
-
-Your message must be well thought through, so first start by detailing your thoughts (which will be private), then type a markdown seperator (aka \`---\`), then all the text following will be your public argument
-Your thoughts may be as long as you want, however your argument must be no more than 2 sentences long.
-You should always make an attempt to address previous arguments whilst also bringing up new ones.
-
-You MUST have a thinking section, then a separator (---), then your public argument.
-You MUST adhere to your debate role in every message
-`;
-}
-
-async function generateNextMessage(
-  params: ArgumentParams,
-  messages: DebateMessage[],
-): Promise<DebateMessage> {
-  const isInitialSide = messages.length % 2 == 0;
-
-  const side = isInitialSide
-    ? params.startingSide
-    : flipSide(params.startingSide);
-
-  const sysMessage = buildSysMessage({
-    side: side,
-    topic: params.topic,
-    messageCount: params.messageCount,
-    currentRole: isInitialSide ? params.role1 : params.role2,
-    otherRole: isInitialSide ? params.role2 : params.role1,
-  });
-
-  const aiMessages = messages.map<ChatCompletionMessageParam>((message, i) => {
-    const isInitialSide = i % 2 == 0;
-    const messageSide = isInitialSide
-      ? params.startingSide
-      : flipSide(params.startingSide);
-    return {
-      role: messageSide == side ? "assistant" : "user",
-      content: `${message.thinking}\n\n---\n\n${message.message}`,
-    };
-  });
-
-  const openAIResponse = await openai.chat.completions.create({
-    messages: [{ role: "system", content: sysMessage }, ...aiMessages],
-    model: "gpt-4",
-  });
-  const responseText = openAIResponse.choices[0].message.content;
-
-  let message = "";
-  let thinking = "";
-  let parts = responseText!.split("---").map((t) => t.trim());
-  if (parts.length == 1) {
-    message = parts[0];
-  } else {
-    thinking = parts[0];
-    message = parts[1];
-  }
-
-  return { message: message, side: side, thinking: thinking };
-}
-
+//Testing if OpenAI's API worked, delete later
 async function main() {
-  console.log("starting debate, please wait...");
-  const params: ArgumentParams = {
-    role1: "philosopher",
-    role2: "young child",
-    messageCount: 2,
-    topic: "Life isn't worth living",
-    startingSide: "for",
-  };
+  const completion = await openai.chat.completions.create({
+    messages: [{ role: 'user', content: 'Write a name for a superhero dog.' }],
+    model: 'gpt-3.5-turbo',
+  });
 
-  const messages: DebateMessage[] = [];
-  for (let i = 0; i < params.messageCount * 2; i++) {
-    const nextMessage = await generateNextMessage(params, messages);
-    messages.push(nextMessage);
-    console.log(nextMessage.side);
-    console.log("thinking:", nextMessage.thinking);
-    console.log("---------------");
-    console.log(nextMessage.message);
-    console.log();
-  }
+  console.log(completion.choices);
 }
 
 main();
