26ffa6e427997d5252e7bfd6952ee594902579e6
AKhil S Kasina <akhil.s.kasina@student.uts.edu.au>
2023-09-19 23:41:35 +1000

    Min Page CSS Fine Tuning

--- packages/client/src/pages/MainPage.tsx
+++ packages/client/src/pages/MainPage.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+//import { useState } from "react";
 import { trpc } from "../utils/trpc";
 
 export function MainPage() {
@@ -13,29 +13,17 @@ export function MainPage() {
         <a href="https://vitejs.dev" target="_blank"></a>
         <a href="https://react.dev" target="_blank"></a>
       </div>
-<<<<<<< HEAD
+
       <body>
 
 
         <div id="footer" >
           <button>Publish</button>
-          <textarea name="Enter Topic" rows={2} cols={50} placeholder="Enter Topic Here...."></textarea>
+          <textarea name="Enter Topic" placeholder="Enter Topic Here...."></textarea>
         </div>
       </body>
-=======
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs bg-red-600">
-        Click on the Vite and React logos to learn more
-      </p>
->>>>>>> 1546544716855bc03cf2b9cb88f44e9b39228467
+
+
       <p>{hello.data}</p>
     </>
   );
