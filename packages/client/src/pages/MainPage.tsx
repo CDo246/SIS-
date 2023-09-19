fe130fd0a1409f86a14af09c5124dcc3f920b43d
james-lynch-1 <110513783+james-lynch-1@users.noreply.github.com>
2023-09-20 00:55:26 +1000

    Merge branch 'master' of https://github.com/CDo246/SIS-Debate into navbar

--- packages/client/src/pages/MainPage.tsx
+++ packages/client/src/pages/MainPage.tsx
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 //import { useState } from "react";
-=======
-// import { useState } from "react";
->>>>>>> acc6622e074476cf21fdc4832de418315d4a444f
 import { trpc } from "../utils/trpc";
 
 export function MainPage() {
@@ -14,20 +10,9 @@ export function MainPage() {
         <a href="https://vitejs.dev" target="_blank"></a>
         <a href="https://react.dev" target="_blank"></a>
       </div>
-<<<<<<< HEAD
-
-=======
->>>>>>> acc6622e074476cf21fdc4832de418315d4a444f
       <body>
         <div id="footer">
           <button>Publish</button>
-<<<<<<< HEAD
-          <textarea name="Enter Topic" placeholder="Enter Topic Here...."></textarea>
-        </div>
-      </body>
-
-
-=======
           <textarea
             name="Enter Topic"
             rows={2}
@@ -36,7 +21,6 @@ export function MainPage() {
           ></textarea>
         </div>
       </body>
->>>>>>> acc6622e074476cf21fdc4832de418315d4a444f
       <p>{hello.data}</p>
     </>
   );
