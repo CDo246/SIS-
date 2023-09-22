d9ae0ebe6e0d34c5f6c60569290ff8746b2b92c8
RichieNguyen <richiepnguyen@gmail.com>
2023-09-22 15:31:19 +1000

    formatting

--- packages/client/src/assets/TypingText.tsx
+++ packages/client/src/assets/TypingText.tsx
@@ -1,30 +1,27 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 // Inputs
 // text: Desired text to display
 // speed: Speed of text being displayed (in miliseconds per character)
-function TypingText({ text, speed }) { 
-    
-    const [displayedText, setDisplayedText] = useState('');
-    const [currentIndex, setCurrentIndex] = useState(0);
+function TypingText({ text, speed }) {
+  const [displayedText, setDisplayedText] = useState("");
+  const [currentIndex, setCurrentIndex] = useState(0);
 
-    useEffect(() => {
-
-        const typingInterval = setInterval(() => {
-            if (currentIndex < text.length) {
-
-                setDisplayedText(text.slice(0, currentIndex + 1));
-                setCurrentIndex(currentIndex + 1);
-            } else {
-                clearInterval(typingInterval);
-        }
+  useEffect(() => {
+    const typingInterval = setInterval(() => {
+      if (currentIndex < text.length) {
+        setDisplayedText(text.slice(0, currentIndex + 1));
+        setCurrentIndex(currentIndex + 1);
+      } else {
+        clearInterval(typingInterval);
+      }
     }, speed);
 
     return () => {
-        clearInterval(typingInterval);
+      clearInterval(typingInterval);
     };
-    }, [currentIndex, text, speed]);
+  }, [currentIndex, text, speed]);
 
-    return <span>{displayedText}</span>;
+  return <span>{displayedText}</span>;
 }
 
 export default TypingText;
