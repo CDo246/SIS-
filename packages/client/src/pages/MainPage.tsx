977fea7c57fe3de110c80d392a6880720bef37e8
james-lynch-1 <110513783+james-lynch-1@users.noreply.github.com>
2023-09-25 12:07:52 +1000

    trim whitespace from submitted topic adjust width of message box and messages improve topic display can no longer submit empty topic

--- packages/client/src/pages/MainPage.tsx
+++ packages/client/src/pages/MainPage.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import "../index.css";
 import TypingText from "../assets/TypingText";
 import leftAvatar from "./left-avatar.jpg";
@@ -26,15 +26,21 @@ export function MainPage() {
       setMessage("");
     }
   };
-  const handleSubmit = (e: React.ChangeEvent<string>) => {
+  const handleSubmit = (
+    e:
+      | React.KeyboardEvent<HTMLTextAreaElement>
+      | React.FormEvent<HTMLFormElement>
+  ) => {
     e.preventDefault();
-    setTopic(submittedTopic);
-    const field = document.getElementById("TopicField") as HTMLInputElement;
-    field.value = "";
+    if (submittedTopic) {
+      setTopic(submittedTopic.trim());
+      const field = document.getElementById("TopicField") as HTMLInputElement;
+      field.value = "";
+    }
   };
   return (
     <>
-      <div className="mx-auto lg:w-8/12 mb-28 p-4">
+      <div className="mx-auto lg:w-7/12 lg:min-w-[900px] mb-24 p-4">
         <div className="border min-h-[65vh] overflow-y-auto dark:border-gray-500 rounded-lg p-4 shadow-md grow">
           {messages.map((msg, index) => (
             <div
@@ -55,7 +61,7 @@ export function MainPage() {
                   msg.isRight
                     ? "bg-blue-100 dark:bg-blue-950 dark:text-white"
                     : "bg-gray-100 dark:bg-gray-800 dark:text-white"
-                } p-2 rounded-lg inline-block lg:max-w-2xl max-w-lg`}
+                } p-2 rounded-lg inline-block max-w-xl`}
               >
                 <TypingText text={msg.text} speed={35} />{" "}
                 {/* Speed can be adjusted to be faster/slower if needed - lower number is faster*/}
@@ -71,7 +77,12 @@ export function MainPage() {
           ))}
         </div>
         <p className="dark:text-white my-2 text-center">
-          <span className="font-bold">Topic:</span> {topic}
+          {topic && (
+            <span className="font-bold" id="TopicSpan">
+              Topic:
+            </span>
+          )}{" "}
+          {topic}
         </p>
         {/* <div className="mt-4">
           <input
@@ -94,7 +105,7 @@ export function MainPage() {
           name="TopicForm"
           id="TopicForm"
           onSubmit={handleSubmit}
-          className="fixed shadow-lg lg:w-1/2 lg:h-20 h-28 w-11/12 m-2 p-2 space-x-2 justify-center rounded-md bottom-0 bg-white/75 dark:bg-gray-800/75 flex"
+          className="fixed shadow-lg lg:w-1/2 lg:h-20 h-24 w-11/12 m-2 p-2 space-x-2 justify-center rounded-md bottom-0 bg-white/75 dark:bg-gray-800/75 flex"
         >
           <textarea
             name="Enter Topic"
@@ -102,7 +113,7 @@ export function MainPage() {
             placeholder="Enter Topic Here...."
             onChange={(e) => setSubmittedTopic(e.target.value)}
             onKeyDown={(e) => {
-              if (e.keyCode == 13 && !e.shiftKey) handleSubmit(e);
+              if (e.key == "Enter" && !e.shiftKey) handleSubmit(e);
             }}
             className="resize-none max-w-screen-lg flex-grow box-border bg-transparent dark:text-white"
           ></textarea>
