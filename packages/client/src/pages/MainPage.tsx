85c80dcff557866dbb4d65464d2d6f7b21110ef9
AKhil S Kasina <akhil.s.kasina@student.uts.edu.au>
2023-10-04 07:11:19 +1100

    version contol

--- packages/client/src/pages/MainPage.tsx
+++ packages/client/src/pages/MainPage.tsx
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-//import { useState } from "react";
-=======
 // import { useState } from "react";
->>>>>>> acc6622e074476cf21fdc4832de418315d4a444f
 import { trpc } from "../utils/trpc";
 
 export function MainPage() {
@@ -14,29 +10,19 @@ export function MainPage() {
         <a href="https://vitejs.dev" target="_blank"></a>
         <a href="https://react.dev" target="_blank"></a>
       </div>
-<<<<<<< HEAD
 
-=======
->>>>>>> acc6622e074476cf21fdc4832de418315d4a444f
+
+
+
       <body>
         <div id="footer">
           <button>Publish</button>
-<<<<<<< HEAD
+
           <textarea name="Enter Topic" placeholder="Enter Topic Here...."></textarea>
         </div>
       </body>
 
 
-=======
-          <textarea
-            name="Enter Topic"
-            rows={2}
-            cols={50}
-            placeholder="Enter Topic Here...."
-          ></textarea>
-        </div>
-      </body>
->>>>>>> acc6622e074476cf21fdc4832de418315d4a444f
       <p>{hello.data}</p>
     </>
   );
